bd3f2d53695f9fdbc8550506fe671a8669d105a6
marianorigo18 <marianowagnerrigo@gmail.com>
2022-12-10 11:02:29 -0300

    se agregaron modificaiones

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const formulario = document.querySelector('#formulario');
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
-
+    
     inputEmail.addEventListener('blur', validar)
     inputAsunto.addEventListener('blur', validar)
     inputMensaje.addEventListener('blur', validar)
@@ -18,6 +19,8 @@ document.addEventListener('DOMContentLoaded', function(){
     function mostrarAlerta(){
         const error = document.createElement('P');
         error.textContent = 'Hubo un Error';
-        console.log(error)
+        error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
+
+        formulario.appendChild(error)
     }
 })
